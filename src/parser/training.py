c3c07e5a388f327f468b206bbbf968961a286883
Guo Yuwei <ywguo@gpugate1.gpufarm.cs.hku.hk>
2022-08-12 10:18:38 +0800

    update

--- src/parser/training.py
+++ src/parser/training.py
@@ -1,8 +1,8 @@
 import os
 import yaml
 import argparse
+import torch
 
-from traitlets import default
 
 def add_training_options(parser):
     group = parser.add_argument_group('Training options')
@@ -14,6 +14,14 @@ def add_training_options(parser):
     group.add_argument("--lr_step_size", type=int, default=10, help="Lr step size for scheduler")
     group.add_argument("--overfit", default=False, action="store_true", help="enable overfit training test")
     group.add_argument("--overfit_size", type=int, default=20, help="size of the overfit training set")
+
+    group.add_argument("--no_val_loss", default=False, action="store_true", help="do not draw loss curve on val set")
+
+    group.add_argument("--checkpoint_step", type=int, default=5, help="save checkpoint per [step] epochs")
+
+    group.add_argument("--num_attentionLayer", type=int, default=4, help="num of attention layer of the encoder/decoder")
+    group.add_argument("--lr_scheduler", type=str, default=None, help="type of the lr scheduler")
+
 
     group.add_argument("--folder", type=str, default="./checkpoint", help="folder to save the checkpoint")
     group.add_argument("--exp_name", type=str, help="name of the experiment")
@@ -29,6 +37,10 @@ def parser():
     args = parser.parse_args()
 
     parameters = {k: v for k, v in vars(args).items()}
+    if torch.cuda.is_available():
+        parameters.update({'device': 'cuda'})
+    else:
+        parameters.update({'device': 'cpu'})
 
     parameters['exp_path'] = os.path.join(parameters['folder'], parameters['exp_name'])
 
---
+++ src/train/train.py
@@ -0,0 +1,106 @@
+import sys
+sys.path.append('.')
+
+import torch
+from src.utils.collate_fn_coco import collate_fn_coco
+from src.utils.get_model_and_data import get_model_and_data
+from tqdm import tqdm
+import os
+from src.parser.training import parser
+from src.utils.eval_loss import eval_loss
+
+from torch.utils.tensorboard import SummaryWriter
+
+
+def do_epoch(model, datasets, parameters, optimizer, scheduler, start_epoch=1):
+
+    # config dataloader
+    train_loader = torch.utils.data.DataLoader(datasets['train'], batch_size=parameters['batch_size'], shuffle=True, collate_fn=collate_fn_coco)
+    val_loader = torch.utils.data.DataLoader(datasets['val'], batch_size=20, collate_fn=collate_fn_coco)
+    num_batch = len(train_loader)
+
+    model.train()
+
+    os.makedirs(parameters['exp_path'], exist_ok=True)
+
+    writer = SummaryWriter(log_dir=parameters['exp_path'])    
+
+    print('Start training...')
+    for eps in range(start_epoch, start_epoch + parameters['num_epochs']):
+
+        for i, batch in enumerate(tqdm(train_loader, desc=f'Epoch {eps}')):
+            
+            for k in batch.keys():
+                if torch.is_tensor(batch[k]): batch[k] = batch[k].to(parameters['device'])
+
+            optimizer.zero_grad()
+            batch = model(batch)
+            mixed_loss, losses = model.compute_loss(batch)
+
+            mixed_loss.backward()
+            optimizer.step()
+
+            if (i % 10 == 0) or (i == num_batch-1):
+                writer.add_scalars(f"Loss/Iters", losses, (eps-1)*num_batch + i)
+
+        if scheduler != None:
+            writer.add_scalar(f"Lr/Epochs", scheduler.get_last_lr()[0], eps)
+        writer.flush()
+
+        # update learning rate
+        if scheduler != None: scheduler.step()
+
+        # draw loss curve on val set
+        if (eps % 5) == 0 or eps == start_epoch+parameters['num_epochs']-1:
+            if not parameters['no_val_loss']:
+                val_losses = eval_loss(val_loader, model, parameters['device'])
+                writer.add_scalars("Loss/Iters", val_losses, (eps-1)*num_batch + i)
+
+        # save checkpoint
+        if (eps % parameters['checkpoint_step']) == 0 or eps == start_epoch+parameters['num_epochs']-1:
+
+            checkpoint = {
+                'epoch': eps,
+                'model': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                # 'lr_scheduler': scheduler.state_dict()
+            }
+            if scheduler != None: checkpoint.update({'lr_scheduler': scheduler.state_dict()})
+            torch.save(checkpoint, os.path.join(parameters['exp_path'], f'checkpoint-epoch{eps}.pth.tar'))
+
+
+if __name__ == '__main__':
+
+    parameters = parser()
+
+    model, datasets = get_model_and_data(parameters)
+
+    # training setting
+    # optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=parameters['lr'])
+    optimizer = torch.optim.AdamW(filter(lambda p: p.requires_grad, model.parameters()), lr=parameters['lr'])
+
+    # lr scheduler
+    if parameters['lr_scheduler'] == 'StepLR':
+        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=parameters['lr_step_size'], gamma=parameters['lr_gamma'], verbose=True)
+    else:
+        scheduler = None
+
+
+    if parameters['checkpoint_path'] != None:
+        checkpoint = torch.load(parameters['checkpoint_path'])
+
+        model.load_state_dict(checkpoint['model'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+
+        if scheduler != None: scheduler.load_state_dict(checkpoint['lr_scheduler'])
+            
+        start_epoch = checkpoint['epoch'] + 1
+        print(f"Model loaded from checkpoint {parameters['checkpoint_path']}...")
+
+    else:
+        start_epoch = 1
+        print('Train on new model...')
+    
+    # print(f'Init lr: {scheduler.get_last_lr()[0]}')
+    do_epoch(model=model, datasets=datasets, parameters=parameters, optimizer=optimizer, scheduler=scheduler, start_epoch=start_epoch)
+
